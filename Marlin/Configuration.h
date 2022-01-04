2c5c8fda60224cb56480cf0b8c4b8e4cdac18a50
Peter Ivanov <ivanovp@gmail.com>
2022-01-04 14:38:34 +0100

    Bed size and X minimum position were corrected.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1384,10 +1384,10 @@
 
 // The size of the printable area
 #define X_BED_SIZE 200
-#define Y_BED_SIZE 180
+#define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -2
+#define X_MIN_POS -7
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
